62d2f7da20e4ea627743b15b927685b791d14e47
REWANELTOBGY <rewaneltobgy@gmail.com>
2025-03-11 22:08:03 +0200

    done

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,1 +1,52 @@
-// Write the codes in here
+// using let , const and var
+
+let age = 2;
+console.log(age);
+
+var name = 'rewan';
+console.log(name);
+
+const hight = 170;
+console.log(hight);
+
+//using arthimetic operators
+
+let x = 20;
+let y = 10; 
+
+//Addition
+console.log(x + y);
+//subtraction
+console.log(x - y);
+//Multiplication
+console.log(x * y);
+//Division
+console.log(x / y);
+//Modulus
+console.log(x % y);
+//Exponential
+console.log(x ** y);
+//Increment
+console.log(x++);
+//decrement
+console.log(y--);
+
+
+//using logical operators
+a = true;
+b = false;
+
+//AND
+console.log(a && b);
+//OR
+console.log(a || b);
+//NOT
+console.log(!a);
+console.log(!b);
+
+// convert num to string 
+let boys = 33
+let strboys = boys.toString();
+console.log(typeof strboys, strboys);
+
+
